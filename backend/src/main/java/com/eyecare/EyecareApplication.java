f5e787e85652ac7e956ed4cd44668f7783b0746f
dopamine49 <nguyengiahaoqt@gmail.com>
2026-04-08 08:01:39 +0700

    Update EyecareApplication.java

--- backend/src/main/java/com/eyecare/EyecareApplication.java
+++ backend/src/main/java/com/eyecare/EyecareApplication.java
@@ -3,7 +3,7 @@ package com.eyecare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EyecareApplication {
 
     public static void main(String[] args) {
